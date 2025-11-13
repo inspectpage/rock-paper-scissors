690b4e71ff9ff6e0c9939007755ea663f1c1123c
Thuan Le <106334856+inspectpage@users.noreply.github.com>
2025-11-13 15:39:16 -0500

    Added a function to play the game and to play a round

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -23,3 +23,48 @@ function getHumanChoice(){
     let Humanchoice = prompt();
     return Humanchoice;
 }
+
+function playGame(){
+    let humanScore = 0
+    let computerScore = 0;
+    
+    function playround(computer, human){
+    human = human.toLowerCase();
+    console.log(`Computer chose:${computer} vs Human chose:${human}`)
+    if (computer == human) {
+        console.log("Tie!")
+    }
+        else if (computer == "rock" && human == "paper"){
+            humanScore += 1;
+            console.log("Human Wins! Paper beats Rock!")
+        }
+        else if (computer == "rock" && human == "scissors"){
+            computerScore += 1
+            console.log("Computer Wins! Rock beats Scissors!")
+        }
+        else if (computer == "paper" && human == "scissors"){
+            humanScore += 1;
+            console.log("Human Wins! Scissors beats Paper!")
+        }
+        else if (computer == "paper" && human == "rock"){
+            computerScore += 1
+            console.log("Computer Wins! Paper beats Rock!")
+        }
+        else if (computer == "scissors" && human == "rock"){
+            humanScore += 1;
+            console.log("Human Wins! Rock beats Scissors!")
+        }
+        else if (computer == "scissors" && human == "paper"){
+            computerScore += 1
+            console.log("Computer Wins! Scissors beat Paper")
+        }
+    }
+    playround(getComputerChoice(), getHumanChoice())
+    playround(getComputerChoice(), getHumanChoice())
+    playround(getComputerChoice(), getHumanChoice())
+    playround(getComputerChoice(), getHumanChoice())
+    playround(getComputerChoice(), getHumanChoice())
+    console.log(`Computer Scored ${computerScore}\nHuman Scored ${humanScore}`)
+}
+
+playGame()
